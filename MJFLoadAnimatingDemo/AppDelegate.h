dc8a0eec5baf9c9a1e0596b5ce665092bf2346c8
巫龙 <454763196@qq.com>
2017-07-25 15:56:38 +0800

    Initial Commit

---
+++ MJFLoadAnimatingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MJFLoadAnimatingDemo
+//
+//  Created by 巫龙 on 17/7/25.
+//  Copyright © 2017年 巫龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MJFLoadAnimatingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MJFLoadAnimatingDemo
+//
+//  Created by 巫龙 on 17/7/25.
+//  Copyright © 2017年 巫龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
